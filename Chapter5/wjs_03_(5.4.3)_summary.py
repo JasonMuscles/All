fd889a5a5728ffdbf82120fefa264ec4f5e1d866
JasonMuscles <wangjinsenufo@qq.com>
2020-03-11 22:47:03 +0800

    第五章 5-7完

---
+++ Chapter5/wjs_03_(5.4.3)_summary.py
@@ -0,0 +1,6 @@
+# Page-79（5-8）
+# Make a list of five or more user names, including the name 'admin'.
+# Imagine you are writing code that will print a greeting to each user after they log in to a website.
+# Loop through the list, and print a greeting to each user:
+# • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+# • Otherwise, print a generic greeting, such as Hello Eric, thank you for login in again.
