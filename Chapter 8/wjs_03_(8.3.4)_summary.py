f49bb8e3699530a9d6e89d49c22380defa587709
JasonMuscles <wangjinsenufo@qq.com>
2020-03-19 22:24:45 +0800

    第八章 129

--- Chapter 8/wjs_03_(8.3.4)_summary.py
+++ Chapter 8/wjs_03_(8.3.4)_summary.py
@@ -1,22 +1,112 @@
-# Page-121（8-5）
+# Page-125（8-6）
 # Write a function called city_country() that takes in the name of a city and its country.
 # The function should return a string formatted like this:“Santiago, Chile”
 # Call your function with at least three city-country pairs, and print the value that’s returned.
 
 
-def city_country(city, country):
-
-    return city.title(), country.title()
-
-
-city1 = city_country("beijing", "china")
-print(city1)
-
-
+# def city_country(city, country):
+#
+#     return city.title(), country.title()
+#
+#
+# city1 = city_country("beijing", "china")
+# print(city1)
 
 
+# Page-125（8-7）
+# Write a function called make_album() that builds a dictionary describing a music album.
+# The function should take in an artist name and an album title, and it should return a dictionary containing
+# these two pieces of information. Use the function to make three dictionaries representing different albums.
+# Print each return value to show that the dictionaries are storing the album information correctly.
+# Add an optional parameter to make_album() that allows you to store the number of tracks on an album.
+# If the calling line includes a value for the number of tracks, add that value to the album’s dictionary.
+# Make at least one new function call that includes the number of tracks on an album.
 
 
+# def make_album(artist_name, album_title):
+#
+#     music_album = {
+#         "artist_name": artist_name,
+#         "album_title": album_title
+#         }
+#
+#     return music_album
+#
+#
+# album = make_album("jason", "bird")
+# print(album)
+#
+# album = make_album("jason1", "bird1")
+# print(album)
+#
+# album = make_album(album_title="jason", artist_name="bird")
+# print(album)
+
+# ==============================================================================================
+
+# def make_album(artist_name, album_title, tracks=0):
+#
+#     music_album_dict = {
+#         "artist_name": artist_name,
+#         "album_title": album_title
+#         }
+#
+#     if tracks:
+#         music_album_dict["tracks"] = tracks
+#
+#     return music_album_dict
+#
+#
+# album = make_album("jason", "bird")
+# print(album)
+#
+# album = make_album("jason1", "bird1")
+# print(album)
+#
+# album = make_album(album_title="jason", artist_name="bird", tracks=0)
+# print(album)
+
+
+# Page-126（8-8）
+# Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title.
+# Once you have that information, call make_album() with the user’s input and print the dictionary that’s created.
+# Be sure to include a quit value in the while loop.
+
+
+def make_album(artist_name, album_title, tracks=0):
+
+    music_album_dict = {
+        "artist_name": artist_name,
+        "album_title": album_title
+        }
+
+    if tracks:
+        music_album_dict["tracks"] = tracks
+
+    return music_album_dict
+
+
+prompt_artist_name = "\ninput artist name："
+prompt_album_title = "\ninput album title："
+
+# print("Enter 'quit' at any time to stop. ")
+
+while True:
+    print("Enter 'quit' at any time to stop. ")
+
+    title = input(prompt_artist_name)
+    if title == "quit":
+        break
+
+    name = input(prompt_album_title)
+    if name == "quit":
+        break
+
+    album = make_album(name, title)
+
+    print(album)
+
+print("\nThanks for responding!")
 
 
 
---
+++ Chapter 8/wjs_04_(8.4.2)_summary.py
@@ -0,0 +1,105 @@
+# Page-129（8-9）
+# Make a list of magician’s names. Pass the list to a function called show_magicians(),
+# with prints the name of each magician in the list.
+
+
+# magician_name_list = ["Jason", "Tom", "Harry"]
+#
+#
+# def show_magicians(magicians):
+#     return print("magician's name:" + str(magicians))
+#
+#
+# show_magicians(magician_name_list)
+
+# ========================================================================================
+
+# def show_magicians(magicians):
+#     for magician in magicians:
+#         print(magician.title())
+#
+#
+# magicians_name_list = ["JASON", "tom", "Marry", "HArry"]
+#
+# show_magicians(magicians_name_list)
+
+
+# Page-129（8-10）
+# Start with a copy of your program from Exercise 8-9.
+# Write a function called make_great() that modifies the list of magicians by adding the phrase
+# the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+
+
+# def show_magicians(magicians):
+#     for magician in magicians:
+#         print(magician)
+#
+#
+# def make_great(magicians):
+#
+#     great_magicians = []
+#
+#     while magicians:
+#         magician = magicians.pop()
+#         great_magician = magician + " the Great"
+#         great_magicians.append(great_magician)
+#
+#     for great_magician in great_magicians:
+#         magicians_list.append(great_magician)
+#
+#
+# magicians_list = ["JASON", "tom", "Marry", "HArry"]
+# show_magicians(magicians_list)
+# print("-------")
+#
+# make_great(magicians_list)
+# show_magicians(magicians_list)
+
+
+# Page-129（8-11）
+# Start with your work from Exercise 8-10.
+# Call the function make_great() with a copy of the list of magicians’ names.
+# Because the original list will be unchanged, return the new list and store it in a separate list.
+# Call show_magicians() with each list to show that you have one list of the original names and one list with
+# the Great added to each magician’s name.
+
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
+
+def make_great(magicians_list):
+
+    great_magicians = []
+
+    while magicians_list:
+        magician = magicians_list.pop()
+        great_magician = magician + " the Great"
+        great_magicians.append(great_magician)
+
+    for great_magician in great_magicians:
+        magicians_list.append(great_magician)
+
+    return magicians_list
+
+
+magicians_list = ["JASON", "tom", "Marry", "HArry"]
+show_magicians(magicians_list)
+print("-------")
+
+make_greats = make_great(magicians_list[:])
+show_magicians(make_greats)
+print("-------")
+
+show_magicians(magicians_list)
+
+
+
+
+
+
+
+
+
+
