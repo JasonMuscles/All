4773da77ae5d1fc2427da301434fbb24491166ee
JasonMuscles <wangjinsenufo@qq.com>
2020-03-23 23:17:11 +0800

    第九章

---
+++ Chapter_9/wjs_05_9_5_0_summary.py
@@ -0,0 +1,15 @@
+# Page-160（9-13）
+# Start with Exercise 6-4 (page 108), where you used a standard dictionary to represent a glossary.
+# Rewrite the program using the OrderedDict class and make sure the order of the output matches the order in
+# which key-value pairs were added to the dictionary.
+
+
+
+
+
+# Page-160（9-14）
+# The module random contains functions that generate random numbers in a variety of ways. The function randint()
+# returns an integer in the range you provide. the following code returns a number between 1 and 6:from random import
+# randint x = randint(1, 6) Make a class Die with one attribute called sides, which has a default value of 6.
+# Write a method called roll_die() that prints a random number between 1 and the number of sides the die has.
+# Make a 6-sided die and roll it 10 times.
